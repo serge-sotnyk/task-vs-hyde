46c1149b78411d76a0156e4848b0cf1f5dc73671
Serge <serge.sotnyk@gmail.com>
2024-12-21 21:06:43 +0200

    Small subset of the all questions

--- select_questions.py
+++ select_questions.py
@@ -1,6 +1,7 @@
 # this script prepares list of questions for selecting with prompt
 from pathlib import Path
 import csv
+import random
 
 from pydantic import BaseModel
 
@@ -24,6 +25,13 @@ def store_questions_csv(questions: list[Question], output_path: Path):
         writer.writerows(qs)
 
 
+def select_questions(questions: list[Question], n: int, output_path: Path):
+    if n > len(questions):
+        raise ValueError(f"n={n} > len(questions)={len(questions)}")
+    res = random.sample(questions, n)
+    store_questions_csv(res, output_path)
+
+
 def main():
     ds = read_ds(ds_root)
     print(f"Total fragments: {len(ds)}")
@@ -41,6 +49,9 @@ def main():
     csv_fname = ds_root / "questions.csv"
     store_questions_csv(questions, csv_fname)
     print(f"Questions stored in {csv_fname}")
+
+    print("Selecting random subset of (200) questions...")
+    select_questions(questions, 200, ds_root / "questions_selected.csv")
 
 
 if __name__ == "__main__":
